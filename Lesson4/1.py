887bccef7ea66421c5ec47b3a47daeaba1a5a012
LevchenkovAlexander <allevchenkov23@gmail.com>
2022-10-16 23:00:49 +0530

    Lesson4

---
+++ Lesson4/1.py
@@ -0,0 +1,27 @@
+month = int(input())
+print("Вы родились в", end=' ')
+if month == 1:
+    print("январе")
+elif month == 2:
+    print("феврале")
+elif month == 3:
+    print("марте")
+elif month == 4:
+    print("апреле")
+elif month == 5:
+    print("мае")
+elif month == 6:
+    print("июне")
+elif month == 7:
+    print("июле")
+elif month == 8:
+    print("августе")
+elif month == 9:
+    print("сентябре")
+elif month == 10:
+    print("октябре")
+elif month == 11:
+    print("ноябре")
+elif month == 12:
+    print("декабре")
+
