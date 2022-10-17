c959d877dc3236b7f59a3f7a1967a7aa49da3f02
LevchenkovAlexander <allevchenkov23@gmail.com>
2022-10-17 23:00:02 +0530

    Lesson4

--- Lesson4/7.py
+++ Lesson4/7.py
@@ -1,12 +1,12 @@
 x = int(input())
 y = int(input())
-if (x > 0) & (y > 0):
+if x > 0 and y > 0:
     print("I")
-elif (x < 0) & (y > 0):
+elif x < 0 and y > 0:
     print("II")
-elif (x < 0) & (y < 0):
+elif x < 0 and y < 0:
     print("III")
-elif (x > 0) & (y < 0):
+elif x > 0 and y < 0:
     print("IV")
 else:
     print("лежит на оси координат")
