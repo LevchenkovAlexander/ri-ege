75cec65ed5d1322ccdbc8d7e97e4dc4d6df21542
LevchenkovAlexander <allevchenkov23@gmail.com>
2022-10-17 22:56:20 +0530

    Lesson4

--- Lesson4/3.py
+++ Lesson4/3.py
@@ -1,10 +1,8 @@
-#Я не уверен, что имеется ввиду под тем, что все числа вводятся на одной строке
-
 T = int(input())
 t = int(input())
 humidity = int(input())
 
-if (humidity < 80) & (t > T):
+if (humidity <= 80) and (t > T):
     print("on")
 else:
     print("off")
