ce11f812ff73b28d55f73b93b572b8811b4c6502
LevchenkovAlexander <allevchenkov23@gmail.com>
2022-11-04 22:55:05 +0530

    intensive

---
+++ intensive/23. Длины строк.py
@@ -0,0 +1,43 @@
+str1 = input()
+str2 = input()
+str3 = input()
+
+if len(str1) < len(str2) and len(str1) < len(str3):
+    if len(str2) < len(str3):
+        if (len(str2) - len(str1)) == (len(str3) - len(str2)):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if (len(str3) - len(str1)) == (len(str2) - len(str3)):
+            print("YES")
+        else:
+            print("NO")
+elif len(str2) < len(str1) and len(str2) < len(str3):
+    if len(str1) < len(str3):
+        if (len(str1) - len(str2)) == (len(str3) - len(str1)):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if (len(str3) - len(str2)) == (len(str1) - len(str3)):
+            print("YES")
+        else:
+            print("NO")
+elif len(str3) < len(str2) and len(str3) < len(str1):
+    if len(str1) < len(str2):
+        if (len(str1) - len(str3)) == (len(str2) - len(str1)):
+            print("YES")
+        else:
+            print("NO")
+    else:
+        if (len(str2) - len(str3)) == (len(str1) - len(str2)):
+            print("YES")
+        else:
+            print("NO")
+elif len(str1) == len(str2) == len(str3):
+    print("YES")
+
+
+
+
