857d634acfcad089042e330beb0e0a6f285ab581
LevchenkovAlexander <allevchenkov23@gmail.com>
2022-11-09 21:34:54 +0530

    intensive

--- intensive/25. Подсчет количества чисел.py
+++ intensive/25. Подсчет количества чисел.py
@@ -3,8 +3,9 @@ b = int(input())
 
 s = 0
 
-for i in range(a, b):
-    if i**3 % 10 == 4 or i**3 % 10 == 9:
+for i in range(a, b+1):
+    j = i**3
+    if j % 10 == 4 or j % 10 == 9:
         s += 1
 
 print(s)
