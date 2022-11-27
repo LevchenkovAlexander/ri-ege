6f40018dfae187a9f4765c66be6bcb5a04bd9f91
LevchenkovAlexander <allevchenkov23@gmail.com>
2022-11-27 17:48:09 +0530

    intensive

--- intensive/38. Палиндром.py
+++ intensive/38. Палиндром.py
@@ -1,10 +1,21 @@
 n = int(input())
-n = str(n)
+
 is_pol = True
-k = len(n) - 1
-for i in range(len(n)):
-    if n[i] != n[k-i]:
+k = 0
+tmp = n
+
+while tmp > 0:
+    tmp //= 10
+    k += 1
+
+_len = k
+
+while k > 0:
+    if (n % 10) != (n // 10**(k - 1)):
         is_pol = False
         break
+    n = n % 10**(k - 1)
+    n //= 10
+    k -= 2
 
 print(is_pol)
