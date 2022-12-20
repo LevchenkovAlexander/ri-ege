ec2ad2c28e38e02a0012067c8d26c66bebf115c8
LevchenkovAlexander <allevchenkov23@gmail.com>
2022-12-20 20:17:25 +0530

    Lesson20

--- Lesson20/1.py
+++ Lesson20/1.py
@@ -1,10 +1,6 @@
 s = input()
-if len(s) % 2 == 0:
-    s1 = s[:len(s)//2]
-    s2 = s[len(s)//2:]
-else:
-    s1 = s[:len(s)//2+1]
-    s2 = s[len(s)//2+1:]
 
+s1 = s[:len(s)//2]
+s2 = s[len(s)//2:]
 
 print(s2, s1)
