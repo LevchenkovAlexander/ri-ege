5f5a04b56c77cc48ccdf6b495e816491a6ba9d82
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-01-16 21:20:12 +0530

    Lesson09

--- Lesson09/3.py
+++ Lesson09/3.py
@@ -1,12 +1,32 @@
-city1 = input()
-city2 = input()
-city3 = input()
+_city1 = input()
+_city2 = input()
+_city3 = input()
 
-if len(city1) > len(city2) > len(city3):
-    print(city1)
-elif len(city2) > len(city1) > len(city3):
-    print(city2)
-else:
-    print(city2)
+city1 = len(_city1)
+city2 = len(_city2)
+city3 = len(_city3)
 
+short_name = ""
+long_name = ""
 
+if city1 > city2 > city3:
+    long_name = _city1
+    if city2 > city3:
+        short_name = _city3
+    else:
+        short_name = _city2
+elif city2 > city1 > city3:
+    long_name = _city2
+    if city1 > city3:
+        short_name = _city3
+    else:
+        short_name = _city1
+elif city3 > city2 > city1:
+    long_name = _city3
+    if city1 > city2:
+        short_name = _city2
+    else:
+        short_name = _city1
+        
+print(short_name)
+print(long_name)
