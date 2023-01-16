92cb27d8ffbfbf57944b8910c241b0f054a9ba39
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-01-16 15:36:06 +0530

    intensive

--- intensive (1 - 57)/28. Делители.py
+++ intensive (1 - 57)/28. Делители.py
@@ -1,8 +1,8 @@
-from math import sqrt, floor
+from math import ceil
 
 n = int(input())
 
-n1 = floor(sqrt(n))
+n1 = ceil(n ** 0.5)
 
 d0 = n
 d1 = 0
@@ -10,7 +10,7 @@ d2 = 0
 c = 0
 
 for i in range(1, n1+1):
-    if n1 % i == 0:
+    if n % i == 0:
         c += 2
         if i > 1 and n // i > d2:
             if n // i > d1:
@@ -18,9 +18,6 @@ for i in range(1, n1+1):
                 d1 = n // i
             else:
                 d2 = n // i
-
-    if i == sqrt(n):
+    if i == n ** 0.5:
         c -= 1
-
-
 print(c, d0, d1, d2)
