652388f840ad0fe9d5ce179bfee4c649f532b8d8
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-01-16 14:55:37 +0530

    intensive

--- intensive (1 - 57)/11. Красивое число.py
+++ intensive (1 - 57)/11. Красивое число.py
@@ -1,6 +1,6 @@
 a = int(input())
 
-if (a // 1000 < 10 and a // 1000 >= 1) and (a % 7 == 0 or a % 17 == 0):
+if (999 < a < 10000) and (a % 7 == 0 or a % 17 == 0):
     print("YES")
 else:
     print("NO")
