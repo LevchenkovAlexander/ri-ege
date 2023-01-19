92872ac5539451b0d95987b7f5231f242769ac27
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-01-19 19:32:56 +0530

    Lesson26

--- Lesson26/4.py
+++ Lesson26/4.py
@@ -2,13 +2,10 @@ def alg(s):
     while "1111" in s:
         s = s.replace("1111", "22", 1).replace("222", "1", 1)
     return s
-        
+
 
 a = []
-b = list()
-for i in range(200, 1000):
+for i in range(201, 1000):
     f = alg("1" * i)
     a.append(f.count("1"))
-    b.append(f)
-print(b)
-print(max(a), a.index(max(a)))
+print(max(a), 201 + a.index(max(a)))
