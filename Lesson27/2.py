531ffcd1556043b098bb3a3027fe35d24c8bb4dc
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-01-20 20:46:50 +0530

    Lesson27

--- Lesson27/2.py
+++ Lesson27/2.py
@@ -1,5 +1,12 @@
 s = input()
-a = [int(i) for i in s if i != " "]
+a = []
+tmp = ""
+for i in range(len(s)):
+    if s[i] != " ":
+        tmp += s[i]
+    if i == len(s) - 1 or s[i] == " ":
+        a.append(int(tmp)) 
+        tmp = ""
 a.sort()
 print(a)
 print(a[::-1])
--- Lesson27/4.py
+++ Lesson27/4.py
@@ -6,10 +6,8 @@ arr = []
 for i in range(len(s)):
     if s[i] != " ":
         temp += s[i]
-        print("=", temp)
     if s[i] == " " or i == len(s) - 1:
         arr.append(int(temp))
-        print(temp)
         temp = ""
         
 index_min = arr.index(min(arr))
