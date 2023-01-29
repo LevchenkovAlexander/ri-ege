3854eaf5a19e2a55ea4c18d28fa599eafb6be387
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-01-29 11:14:24 +0530

    intensive

--- intensive (58 - )/71. Последовательность ABABABA.py
+++ intensive (58 - )/71. Последовательность ABABABA.py
@@ -4,14 +4,14 @@ l = 0
 f = ""
 
 for i in range(len(s)):
-    if s[i] == "A" and (f == "B" or f == ""):
+    if s[i] == "A":
         l += 1
         f = "A"
     elif s[i] == "B" and f == "A":
         l += 1
         f = "B"
     else:
-        l = 1 
+        l = 0
 
     max_l = l if max_l < l else max_l
 print(max_l)
