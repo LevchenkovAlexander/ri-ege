672007080cfd27770d4f9ffeea19b4d76eb552ec
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-06 21:54:26 +0530

    intensive

--- intensive (58 - )/75. Задача 14 ЕГЭ (2).py
+++ intensive (58 - )/75. Задача 14 ЕГЭ (2).py
@@ -3,5 +3,5 @@ for x in '0123456789a':
         a = int(f'{x}341{y}', 11) + int(f'56{x}1{y}', 19)
         if a % 305 == 0:
             ans = a//305
-            print(a)
+            print(ans)
             break
