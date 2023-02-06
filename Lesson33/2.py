8b02382150ba373906ffa04108de3445748bf185
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-06 20:34:34 +0530

    Lesson33

--- Lesson33/2.py
+++ Lesson33/2.py
@@ -6,6 +6,6 @@ avg = sum(avg_m)/len(avg_m)
 pairs = []
 
 for i in range(len(f) - 1):
-    if (f[i] % 2 == 0 and f[i + 1] < avg) or (f[i + 1] % 2 == 0 and f[i] < avg):
+    if (f[i] % 3 == 0 and f[i + 1] < avg) or (f[i + 1] % 3 == 0 and f[i] < avg):
         pairs.append(f[i] + f[i + 1])
 print(len(pairs), max(pairs))
