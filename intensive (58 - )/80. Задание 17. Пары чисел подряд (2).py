628a13bcffe5d9c8f4870c2612dd1d75a91bb623
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-08 20:58:05 +0530

    intensive

---
+++ intensive (58 - )/80. Задание 17. Пары чисел подряд (2).py
@@ -0,0 +1,13 @@
+def f(x):
+    return x % 10 == 6 and x % 3 == 0
+
+
+file = open('files/80.txt')
+a = list(map(int, file))
+
+p = []
+
+for i in range(len(a) - 1):
+    if f(a[i]) or f(a[i + 1]):
+        p.append(min(a[i], a[i+1]))
+print(len(p), min(p))
