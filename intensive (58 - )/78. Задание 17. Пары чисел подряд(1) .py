a7d20949cbbcc46c92f02f60284b88361064a02b
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-08 20:51:51 +0530

    intensive

--- intensive (58 - )/78. Задание 17. Пары чисел подряд(1) .py
+++ intensive (58 - )/78. Задание 17. Пары чисел подряд(1) .py
@@ -1,10 +1,13 @@
-file = open('files\\78.txt')
+file = open('files/78.txt')
 
 a = list(map(int, file))
+list_171 = [i for i in a if i % 171 == 0]
+max_171 = max(list_171)
 p = []
 
+
 for i in range(len(a) - 1):
-    if (a[i] % 171 == 0 or a[i + 1] % 171 == 0) and (a[i] % 2 or a[i + 1] % 2):
+    if (a[i] < max_171 or a[i + 1] < max_171) and (a[i] % 2 or a[i + 1] % 2):
         p.append(a[i] + a[i + 1])
 print(len(p), max(p))
  
