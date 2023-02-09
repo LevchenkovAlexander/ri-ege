55ca4ac694d84ad7cde78c15c2fe5943a972b206
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-09 20:50:42 +0530

    Lesson09

--- Lesson09/4.py
+++ Lesson09/4.py
@@ -6,12 +6,12 @@ D = b**2 - 4*a*c
 d = D**0.5
 
 if D > 0:
-    x1 = (-b + d)/2*a
-    x2 = (-b - d)/2*a
-    print(x1, x2)
+    x1 = (-b + d)/(2*a)
+    x2 = (-b - d)/(2*a)
+    print(x2, x1)
 
 elif D == 0:
-    x1 = -b/2*a
+    x1 = -b/(2*a)
     print(x1)
 else:
     print("Нет корней")
