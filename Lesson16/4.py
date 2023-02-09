58e8a4c358808239933b861e94c4a0ce76d0f943
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-09 21:05:31 +0530

    Lesson16

--- Lesson16/4.py
+++ Lesson16/4.py
@@ -1,7 +1,7 @@
 k = 0
 
-for x in range(777):
-    for y in range(777):
+for x in range(1, 777):
+    for y in range(1, 777):
         if (12 * x + 13 * y) == 777:
             k += 1
             print(f"{k} : ({x}; {y})")
