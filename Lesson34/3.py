de97ac27d48649d7106f26c99c7c9c665f669f21
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-10 20:51:55 +0530

    Lesson34

--- Lesson34/3.py
+++ Lesson34/3.py
@@ -1,7 +1,7 @@
 a = [int(i) for i in open('Lesson34\\'+input()+'.txt').read().splitlines()]
 p = []
 
-min103 = min( [i for i in a if i % 103] )
+min103 = min( [i for i in a if i % 103 == 0] )
 
 for i in range(len(a) - 1):
     sum_ = a[i] + a[i + 1]
