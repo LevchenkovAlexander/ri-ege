c561ae5772a9a05c6b75ada47557142e7ebb6c7e
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-16 21:56:34 +0530

    intensive

--- intensive (58 - )/86. Задача 24 (1).py
+++ intensive (58 - )/86. Задача 24 (1).py
@@ -1,8 +1,8 @@
-s = open("files\\86.txt").read()
+s = open("files\\test.txt").read()
 
-d = [1 if (i != "K" and i != "L") else 0 for i in s]
+d = [1 for i in s]
 
 for i in range(1, len(s)):
-    if s[i] != "K" and s[i] != "L": d[i] = d[i - 1] + 1
-    
+    if s[i-1]+s[i] not in "KLK": d[i] = d[i - 1] + 1
+
 print(max(d))
