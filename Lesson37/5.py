38f5947716500e7da09b7415c6156826c7d0808f
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-21 08:59:55 +0530

    Lesson37

--- Lesson37/5.py
+++ Lesson37/5.py
@@ -5,10 +5,15 @@ def f(s):
 a = open("Lesson37\\5.txt").read()
 s = ''
 count = 0
-for i in range(len(a)):
+for i in range(len(a)-1):
     s += a[i]
-    if f(s) or i == len(a) - 1:
-        if len(s) > 3:    
+    if f(s+a[i+1]):
+        if len(s) > 2:    
             count += 1
-        s = ''
+            s = ""
+        else: s = a[i]
+    elif i == len(a) - 2 and len(s) > 1: count += 1
+
+            
+                                
 print(count)
