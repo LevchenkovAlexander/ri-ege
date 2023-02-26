543b4d3815de3bff018a23d798660d9d64905b96
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-02-26 15:26:46 +0530

    Lesson38

--- Lesson38/1.py
+++ Lesson38/1.py
@@ -1,10 +1,11 @@
 def dels(num):
-    dels = set()
+    dels_beg = []
+    dels_end = []
     for i in range(2, int(num**0.5) + 1):
         if num % i == 0:
-            dels.add(i)
-            dels.add(num//i)
-    return dels
+            dels_beg.append(i)
+            dels_end.append(num//i)
+    return dels_beg + dels_end[::-1]
 
 for i in range(174457, 174505 + 1):
     d = dels(i)
