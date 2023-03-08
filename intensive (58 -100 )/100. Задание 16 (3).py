d260b31f9169d086cd97d8c273898b0fde9604d8
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-03-08 21:51:35 +0530

    intensive

---
+++ intensive (58 -100 )/100. Задание 16 (3).py
@@ -0,0 +1,14 @@
+def f(n):
+    if n < 10: return n
+    return n % 10 + f(n // 10)
+
+
+def g(n):
+    if n < 10: return n
+    return g(f(n))
+
+
+s = 0
+for i in range(10, 100):
+    s += g(i)
+print(s)
