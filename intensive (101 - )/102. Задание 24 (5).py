3a6220f438f3171984852f7811b1750929539586
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-03-10 22:58:24 +0530

    intensive

---
+++ intensive (101 - )/102. Задание 24 (5).py
@@ -0,0 +1,17 @@
+def max_dist(s):
+    m = []
+    for i in range(len(s)):
+        count = 0
+        for j in range(i+1, len(s)):
+            count += 1
+            if s[i] == s[j]:
+                m.append(count)
+                count = 0
+    return max(m)
+
+
+a = [i for i in open('files\\102.txt').read().splitlines() if i.count("G") < 15]
+mx = []
+for i in a:
+    mx.append(max_dist(i))
+print(max(mx))
