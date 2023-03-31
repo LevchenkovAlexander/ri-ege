f69197b60e3bef1fa5d3b8b9ea4c4a22b64269ab
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-03-31 22:10:19 +0530

    intensive

--- intensive (101 - )/115. Задание 24 (9).py
+++ intensive (101 - )/115. Задание 24 (9).py
@@ -1,19 +1,10 @@
 a = open("files/115.txt").read()
-f = ["0" if i.isalpha() else "1" for i in a]
+f = ["l" if i.isalpha() else "n" for i in a]
 
-c = mx = 0
-fl = 1 if f[0] == 0 else 0
+d = [1 if i == "l" else 0 for i in f]
+
+
 for i in range(1, len(f)):
-    if fl == 1 and f[i-1] + f[i] in "010":
-        c += 1
-        mx = max(mx, c)
-    elif fl == 1:
-        c = 0
-        fl = 0
-    elif f[i] == "0":
-        fl = 1
-        c += 1
-    else:
-        c = 0
-        fl = 0
-print(mx)
+    if f[i-1] + f[i] in "lnl":
+        d[i] = d[i-1] + 1
+print(max(d))
