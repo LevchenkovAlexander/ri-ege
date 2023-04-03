0a3703800e9a6fdde26247db7c22d50085abbb89
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-04-03 22:44:16 +0530

    intensive

---
+++ intensive (101 - )/118. Задание 24 (10).py
@@ -0,0 +1,10 @@
+s = open("files/118.txt").read()
+d = [1 for i in s]
+
+for i in range(1, len(s)):
+    if s[i] == s[i-1]:
+        d[i] = d[i-1] + 1
+
+for i in range(len(d)):
+    if d[i] == max(d):
+        print(s[i], d[i])
