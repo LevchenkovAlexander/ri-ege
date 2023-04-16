4c6a820955882a8bd02b392514c09be54526bfe0
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-04-16 11:07:47 +0530

    Lesson52

--- Lesson52/2.py
+++ Lesson52/2.py
@@ -8,11 +8,7 @@ def func(data, id):
 data = [[j for j in i.split("\t")[:-1]] for i in open("Lesson52/2.txt").read().splitlines()[1:]]
 data = {int(i[0]): {"time" : int(i[1]), "subs" : [int(j) if len(j) else 0 for j in i[2].split(";")]} for i in data}
 
-mx = count = 0
+ans = []
 for i in data:
-    if count < 48:
-        mx = max(mx, func(data, i))
-        count += 1
-    else: break
-    
-print(mx)
+    ans.append(func(data, i))
+print(max(sorted(ans)[:49]))
