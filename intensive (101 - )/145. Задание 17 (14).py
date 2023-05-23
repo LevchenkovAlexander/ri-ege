9cdfb38096ec9fc480fc2c2cc0b83d0c2f134601
LevchenkovAlexander <allevchenkov23@gmail.com>
2023-05-23 20:41:11 +0530

    intensive

---
+++ intensive (101 - )/145. Задание 17 (14).py
@@ -0,0 +1,11 @@
+a = list(map(int, open("files/145.txt")))
+
+mx = max([i for i in a if i % 127])
+count = 0
+mn_sum = float("inf")
+
+for i in range(len(a)-1):
+    if (a[i] > mx or a[i+1] > mx) or ("31" in oct(a[i])[2:] or "31" in oct(a[i+1])[2:]):
+        count += 1
+        mn_sum = min(mn_sum, a[i] + a[i+1])
+print(count, mn_sum)
